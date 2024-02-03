5ef37fbd3b600ede5b927cef1830a15d87a6eb60
Warlie Yap <137013560+warlie3d@users.noreply.github.com>
2024-02-03 15:29:11 -0500

    finished activity

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,17 +1,40 @@
 const router = require('express').Router()
 const db = require("../models")
+const bcrypt = require('bcrypt')
 
 const { User } = db
 
-router.post('/', async (req, res) => {
-    const user = await User.create(req.body)
-    res.json(user)
-})
+// router.post('/', async (req, res) => {
+//     const user = await User.create(req.body)
+//     res.json(user)
+// })
 
+router.post('/', async (req, res) => {
+    try {
+        const { password, ...rest } = req.body;
+        const passwordDigest = await bcrypt.hash(password, 10)
+        const user = await User.create({ 
+            ...rest, 
+            passwordDigest
+        })
+        res.json(user)
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error"})
+    }
+})   
 
 router.get('/', async (req, res) => {
-    const users = await User.findAll()
-    res.json(users)
+    try {
+        const users = await User.findAll()
+        res.json(users)
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Errror"})
+    }
+
 })
+
+
 
 module.exports = router
